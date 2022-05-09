89f20a72d2dd1762b37d76829fe17bfa09e4fc01
Artebomba <artypa85@gmail.com>
2022-05-09 13:53:00 +0300

    Add classes

---
+++ Calculator.py
@@ -0,0 +1,12 @@
+def add(num1, num2):
+    return num1 + num2
+def sub(num1, num2):
+    return num1 - num2
+def multiply(num1, num2):
+    return num1 * num2
+def divide(num1, num2):
+    return num1 / num2
+def modulo(num1):
+    return num1 % 2
+def exponent(num1, num2):
+    return num1 ** num2

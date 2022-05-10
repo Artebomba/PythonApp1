82acd2c1df01d3b57d0eccdc4ca6cf9b2747e9f1
Artebomba <artypa85@gmail.com>
2022-05-10 16:52:24 +0300

    Multithread. changed

--- Multithreading.py
+++ Multithreading.py
@@ -1,19 +1,29 @@
 from threading import *
+import threading
 from time import sleep
+
+lock = threading.Lock()
+
 class Example(Thread):
     def run(self):
-        for i in range(10):
+        for i in range(5):
+            lock.acquire()
             print("Hello from ExampleOne")
             sleep(1)
+            lock.release()
 
 class ExampleTwo(Thread):
     def run(self):
-        for i in range(10):
+        for i in range(5):
+            lock.acquire()
             print("Hello from ExampleTwo")
             sleep(1)
+            lock.release()
             
 example = Example()
 exampleTwo = ExampleTwo()
 example.start()
-sleep(0.1)
 exampleTwo.start()
+example.join()
+exampleTwo.join()
+print("End")
